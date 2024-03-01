edd7c4fd5df1c11d80bca335be746f4264d563ef
enesozmus <ensozmus@gmail.com>
2024-03-01 20:49:37 +0300

    Initial Commit

---
+++ UnitConversions/UnitConversionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConversionsApp.swift
+//  UnitConversions
+//
+//  Created by enesozmus on 1.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct UnitConversionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
